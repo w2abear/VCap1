1ab9f76b42400b7732e75284723259bc1dad3103
Butch Abear <jwbutch@hotmail.com>
2017-07-11 20:58:02 +0800

    initial

--- VCap1/VCap1.cpp
+++ VCap1/VCap1.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
 
+
+using namespace std;
+char key;
 
 int main()
 {
+	cvNamedWindow("Camera_Output", 1);
+	CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);
+
+	while (1) 
+	{
+		IplImage* frame = cvQueryFrame(capture);   //create image from capture
+		cvShowImage("Camera_Output", frame);
+
+		key = cvWaitKey(10);
+		if (char(key)==27) 
+		{
+			break;
+		}
+
+	}
+
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Camera_Output");
+
+
     return 0;
 }
 
